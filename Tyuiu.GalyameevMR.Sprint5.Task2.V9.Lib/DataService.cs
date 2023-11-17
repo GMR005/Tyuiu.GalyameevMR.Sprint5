1bca70290ebf7fa41f9062cc2756ab696723b8ec
GMR005 <mihailgalemeev@mail.ru>
2023-11-18 04:00:40 +0500

    Task2

---
+++ Tyuiu.GalyameevMR.Sprint5.Task2.V9.Lib/DataService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.GalyameevMR.Sprint5.Task2.V9.Lib
+{
+    public class DataService : ISprint5Task2V9
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(path);
+            };
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j] % 2 != 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            string res = "";
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); i++)
+                {
+                    if (j != matrix.GetLength(1) - 1)
+                    {
+                        res += matrix[i, j] + ";";
+                    }
+                    else
+                    {
+                        res += matrix[i, j];
+                    }
+                }
+                if (i != matrix.GetLength(0) - 1)
+                {
+                    File.AppendAllText(path, res + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, res);
+                }
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.GalyameevMR.Sprint5.Task2.V9.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.GalyameevMR.Sprint5.Task2.V9.Lib;
+namespace Tyuiu.GalyameevMR.Sprint5.Task2.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\visual studio\Tyuiu.GalyameevMR.Sprint5\Tyuiu.GalyameevMR.Sprint5.Task1.V27\bin\Debug\OutPutFileTask2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
