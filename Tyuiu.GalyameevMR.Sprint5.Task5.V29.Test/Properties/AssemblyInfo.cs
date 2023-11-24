62460387ebf5d6c0ddc5b17135aa525eafaef05e
GMR005 <mihailgalemeev@mail.ru>
2023-11-24 21:11:52 +0500

    Task5

---
+++ Tyuiu.GalyameevMR.Sprint5.Task5.V29.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GalyameevMR.Sprint5.Task5.V29.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GalyameevMR.Sprint5.Task5.V29.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a45f6305-d51b-422b-adfb-82f7b73102b9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GalyameevMR.Sprint5.Task5.V29/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GalyameevMR.Sprint5.Task5.V29.Lib;
+
+namespace Tyuiu.GalyameevMR.Sprint5.Task5.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            string path = $@"C:\DataSprint5\InPutDataFileTask5V29.txt";
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine( res);
+            Console.ReadKey();
+        }
+    }
+}
