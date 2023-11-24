b7cd859f3d4760e2b3244f876cd6e3029c09f599
GMR005 <mihailgalemeev@mail.ru>
2023-11-25 01:00:01 +0500

    Task2

--- Tyuiu.GalyameevMR.Sprint5.Task2.V9.Lib/DataService.cs
+++ Tyuiu.GalyameevMR.Sprint5.Task2.V9.Lib/DataService.cs
@@ -15,9 +15,9 @@ namespace Tyuiu.GalyameevMR.Sprint5.Task2.V9.Lib
             string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask2.csv";
 
             FileInfo fileinfo = new FileInfo(path);
-            bool fileex = fileinfo.Exists;
+            bool fileExist = fileinfo.Exists;
 
-            if (fileex) File.Delete(path);
+            if (fileExist) File.Delete(path);
 
             int rows = matrix.GetUpperBound(0) + 1;
             int columns = matrix.Length / rows;
@@ -38,7 +38,7 @@ namespace Tyuiu.GalyameevMR.Sprint5.Task2.V9.Lib
                 for (int j = 0; j < columns; j++)
                 {
                     if (j != columns - 1) str = str + matrix[i, j] + ";";
-                    else str = str + matrix[i, j];
+                    else str += matrix[i, j];
                 }
                 if (i != rows - 1) File.AppendAllText(path, str + Environment.NewLine);
                 else File.AppendAllText(path, str);
